b650114d26fdc00f5cf84fd810d56d4126409452
kaushal467 <kaushal@matchbest@software.com>
2025-08-18 22:26:02 +0530

    addport

--- server.js
+++ server.js
@@ -59,4 +59,4 @@ app.get("/api/files", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(5000, "0.0.0.0", () =>  console.log(`🚀 Server running on port ${PORT}`));
